939c5e70ef5619c98c6b1ccf820ddc047a5a480f
dianmustika <mustikacare@gmail.com>
2023-11-22 15:13:13 +0700

    theme oke

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NorekController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +22,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
+/* Route::get('/home', function () {
     return view('home');
-});
+}); */
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/norek', [NorekController::class, 'index'])->name('norek');
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
